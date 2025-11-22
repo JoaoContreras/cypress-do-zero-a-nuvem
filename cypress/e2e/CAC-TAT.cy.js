63f398993545a26d3709f1c83e4028b0b8991674
Joao <jandrecontreras@gmail.com>
2025-11-22 14:39:51 -0300

    Meu primeiro teste automatizado

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,11 @@
+
+//describe define a suite de testes
+describe('Central de Atendimento ao Cliente TAT', () => {
+  //it define o caso de teste.
+  it('verifica o título da aplicação', () => {
+    cy.visit('./src/index.html')
+
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+})
+
