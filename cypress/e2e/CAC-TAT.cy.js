e1d0c0cd083c53d728e77d32f960088a719fc910
Joao <jandrecontreras@gmail.com>
2025-11-24 18:14:35 -0300

    aula 2

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -2,8 +2,8 @@
 //describe define a suite de testes
 describe('Central de Atendimento ao Cliente TAT', () => {
 
-  beforeEach(() => {   
-     cy.visit('./src/index.html')
+  beforeEach(() => {
+    cy.visit('./src/index.html')
 
   })
   //it define o caso de teste.
@@ -14,68 +14,68 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it('preenche os campos obrigatórios e envia o formulário', () => {
-    
 
-  const longText = Cypress._.repeat('Texto longo', 20)
-//preecher nome 
-  cy.get('#firstName').type('João')
-  //preencher sobrenome
-  cy.get('#lastName').type('Contreras')
-  //preencher email
-  cy.get('#email').type('jandrecontreras@gmail.com')
-  //preencher campo de texto
-  cy.get('#open-text-area').type(longText, {delay: 0})
-  //clicar no botão enviar
-  cy.get('button[type="submit"]').click()
 
-  //verificar se a mensagem de sucesso está visível
-  cy.get('.success').should('be.visible')
+    const longText = Cypress._.repeat('Texto longo', 20)
+    //preecher nome 
+    cy.get('#firstName').type('João')
+    //preencher sobrenome
+    cy.get('#lastName').type('Contreras')
+    //preencher email
+    cy.get('#email').type('jandrecontreras@gmail.com')
+    //preencher campo de texto
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    //clicar no botão enviar
+    cy.contains('button', 'Enviar').click()
+
+    //verificar se a mensagem de sucesso está visível
+    cy.get('.success').should('be.visible')
   })
 
-//exercicio extra 2
+  //exercicio extra 2
   it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
-  cy.get('#firstName').type('João')
-  //preencher sobrenome
-  cy.get('#lastName').type('Contreras')
-  //preencher email
-  cy.get('#email').type('jandrecontrerascom')
-  //preencher campo de texto
-  cy.get('#open-text-area').type('Teste de preenchimento de campo ')
-  //clicar no botão enviar
-  cy.get('button[type="submit"]').click()
+    cy.get('#firstName').type('João')
+    //preencher sobrenome
+    cy.get('#lastName').type('Contreras')
+    //preencher email
+    cy.get('#email').type('jandrecontrerascom')
+    //preencher campo de texto
+    cy.get('#open-text-area').type('Teste de preenchimento de campo ')
+    //clicar no botão enviar
+    cy.contains('button', 'Enviar').click()
 
-  //verificar se a mensagem de erro está visível
-  cy.get('.error').should('be.visible')
+    //verificar se a mensagem de erro está visível
+    cy.get('.error').should('be.visible')
 
   })
   //exercicio extra 3
 
   it('campo telefone continua vazio quando preenchido com valor não numérico', () => {
-   cy.get('#phone').type('teste')
-   .should('have.value','')
+    cy.get('#phone').type('teste')
+      .should('have.value', '')
 
   })
   //exercicio extra 4
 
-  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => { 
-cy.get('#firstName').type('João')
-  //preencher sobrenome
-  cy.get('#lastName').type('Contreras')
-  //preencher email
-  cy.get('#email').type('jandrecontrerascom')
-  //marcar checkbox telefone obrigatório
-  cy.get('#phone-checkbox').check()
-  //preencher campo de texto
-  cy.get('#open-text-area').type('Teste de preenchimento de campo ')
-  //clicar no botão enviar
-  cy.get('button[type="submit"]').click()
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('João')
+    //preencher sobrenome
+    cy.get('#lastName').type('Contreras')
+    //preencher email
+    cy.get('#email').type('jandrecontrerascom')
+    //marcar checkbox telefone obrigatório
+    cy.get('#phone-checkbox').check()
+    //preencher campo de texto
+    cy.get('#open-text-area').type('Teste de preenchimento de campo ')
+    //clicar no botão enviar
+    cy.contains('button', 'Enviar').click()
 
-  //verificar se a mensagem de erro está visível
-  cy.get('.error').should('be.visible')
+    //verificar se a mensagem de erro está visível
+    cy.get('.error').should('be.visible')
 
 
   })
-//exercicio extra 5
+  //exercicio extra 5
   it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
     cy.get('#firstName').type('João').should('have.value', 'João').clear().should('have.value', '')
     cy.get('#lastName').type('Contreras').should('have.value', 'Contreras').clear().should('have.value', '')
@@ -86,54 +86,68 @@ cy.get('#firstName').type('João')
   //exercicio extra 6
 
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 
-  //verificar se a mensagem de erro está visível
-  cy.get('.error').should('be.visible')
-})
+    //verificar se a mensagem de erro está visível
+    cy.get('.error').should('be.visible')
+  })
 
-it('envia o formulário com sucesso usando um comando customizado', () => {
-cy.fillMandatoryFieldsAndSubmit()
+  it('envia o formulário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit()
 
-cy.get('.success').should('be.visible')
-
-
-})
-
-it.only('envia o formulário com sucesso usando um comando customizado com dados costumizaveis', () => {
-  
-const data = {
-  firstName: 'Ivete',
-  lastName: 'Maia',
-  email: 'Ivetemaia@example.com',
-  text: 'Teste de preenchimento de campo personalizado'
-}
-cy.fillMandatoryFieldsAndSubmit(data)
-
-cy.get('.success').should('be.visible')
+    cy.get('.success').should('be.visible')
 
 
+  })
+
+  it('envia o formulário com sucesso usando um comando customizado com dados costumizaveis', () => {
+
+    const data = {
+      firstName: 'Ivete',
+      lastName: 'Maia',
+      email: 'Ivetemaia@example.com',
+      text: 'Teste de preenchimento de campo personalizado'
+    }
+    cy.fillMandatoryFieldsAndSubmit(data)
+
+    cy.get('.success').should('be.visible')
+  })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+
+    cy.get('#product').select('Youtube').should('have.value', 'youtube')
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+
+    cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+
+  })
 
 
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
+  it.only('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product').select(1).should('have.value', 'blog')
 
 
 
-
-
+  })
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,15 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data) => {
-cy.get('#firstName').type(data.firstName )
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'John',
+    lastName: 'Wick',
+    email: 'johnwick@example.com',
+    text: 'mensagem de teste default'
+} 
+
+
+) => {
+  cy.get('#firstName').type(data.firstName )
   cy.get('#lastName').type(data.lastName)
   cy.get('#email').type(data.email) 
   cy.get('#open-text-area').type(data.text)
-  cy.get('button[type="submit"]').click()
+   cy.contains('button', 'Enviar').click()
 })
