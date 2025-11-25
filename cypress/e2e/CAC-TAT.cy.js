55eeae33a35ad031d710c327841b448fa289f522
Joao <jandrecontreras@gmail.com>
2025-11-25 15:29:40 -0300

    um nome legal

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -116,7 +116,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('seleciona um produto (YouTube) por seu texto', () => {
 
 
-    cy.get('#product').select('youtube').should('have.value','youtube')
+    cy.get('#product').select('youtube').should('have.value', 'youtube')
   })
 
   it('seleciona um produto (Mentoria) por seu valor (value)', () => {
@@ -135,19 +135,51 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   it('marca o tipo de atendimento "Feedback"', () => {
     cy.get('input[type="radio"][value="feedback"]').check()
-    
-    .should('be.checked')
+
+      .should('be.checked')
   })
 
-  it('marca cada tipo de atendimento', () => { 
+  it('marca cada tipo de atendimento', () => {
 
-cy.get('input[type="radio"]').each($radio => {
-  cy.wrap($radio).check()
-  cy.wrap($radio).should('be.checked')
-})
+    cy.get('input[type="radio"]')
+      .each(typeOfService => {
+        cy.wrap(typeOfService)
+          .check()
+          .should('be.checked')
+      })
 
 
-    
+
+  })
+  it('marca ambos checkboxes, depois desmarca o último', () => {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .should('be.checked')
+      .last()
+      .uncheck()
+      .should('not.be.checked')
+  })
+  it('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('input[type="file"]#file-upload')
+      .should('not.have.value')
+      .selectFile('cypress/fixtures/example.json')
+      .should($input => {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+
+
+  })
+
+
+
+
+  it.only('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should($input => {
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+
   })
 
 })
