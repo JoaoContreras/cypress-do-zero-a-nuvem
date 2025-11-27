6bbcd70944a67e07611640d19bf56765f27fd6b0
Joao <jandrecontreras@gmail.com>
2025-11-27 15:00:26 -0300

    'alteracoes

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -173,7 +173,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
 
 
-  it.only('seleciona um arquivo simulando um drag-and-drop', () => {
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
     cy.get('#file-upload')
       .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
       .should($input => {
@@ -182,8 +182,45 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   })
 
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('#file-upload')
+      .selectFile('@sampleFile')
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should($input => {
+        expect($input[0].files[0].name).to.equal('example.json')
+
+      })
+  })
+it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique',()=>{
+cy.contains('a','Política de P rivacidade')
+.should('have.attr','href','privacy.html',)
+.and('have.attr','target','_blank')
+
+})
+it('acessa a pagina da politica de privacidade removendo o target e entao clicando',()=>{
+cy.contains('a','Política de Privacidade')
+.invoke('removeAttr','target')
+.click()
+
+cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+
 })
 
+it('Testa a página da política de pricacidade de forma independente',()=>{
+cy.contains('a','Política de Privacidade')
+
+
+
+
+})
+
+
+
+
+
+})
 
 
 
